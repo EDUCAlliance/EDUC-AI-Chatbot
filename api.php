fc25d882f8946f14ea6df131ee7b0d109b9071f6
pascalkienast <info@pascalkienast.de>
2025-06-18 00:28:41 +0200

    Update api.php

--- api.php
+++ api.php
@@ -30,6 +30,17 @@ $onboardingManager = new OnboardingManager($db, $logger);
 
 $apiKey = NextcloudBot\env('BOT_TOKEN');
 
+// --- Add CORS headers for cross-origin requests ---
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, X-Api-Key');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 // --- 1. Receive & Verify Webhook ---
 $inputContent = file_get_contents('php://input');
 $headers = getallheaders();
